2cf3cdffa35057ef22a795dfb1c5ebf911029c7b
Jakub Wojtach <jakubwojtach@gmail.com>
2025-05-09 20:44:09 +0200

    Add @sanity/locale-pl-pl package and integrate Polish locale support in Sanity configuration.

--- sanity.config.ts
+++ sanity.config.ts
@@ -4,14 +4,14 @@
  * This configuration is used to for the Sanity Studio that’s mounted on the `/app/studio/[[...tool]]/page.tsx` route
  */
 
-import { visionTool } from '@sanity/vision'
 import { defineConfig } from 'sanity'
 import { structureTool } from 'sanity/structure'
 
 // Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
-import { apiVersion, dataset, projectId } from './src/sanity/env'
+import { dataset, projectId } from './src/sanity/env'
 import { schema } from './src/sanity/schemaTypes'
 import { structure } from './src/sanity/structure'
+import { plPLLocale } from '@sanity/locale-pl-pl'
 
 export default defineConfig({
 	basePath: '/studio',
@@ -19,10 +19,5 @@ export default defineConfig({
 	dataset,
 	// Add and edit the content schema in the './sanity/schemaTypes' folder
 	schema,
-	plugins: [
-		structureTool({ structure }),
-		// Vision is for querying with GROQ from inside the Studio
-		// https://www.sanity.io/docs/the-vision-plugin
-		visionTool({ defaultApiVersion: apiVersion })
-	]
+	plugins: [structureTool({ structure }), plPLLocale()]
 })
